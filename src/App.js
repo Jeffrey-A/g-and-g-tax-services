161b0855a9adde256361a36c76a0890ef8a7731a
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-10-06 00:05:42 -0400

    add img corousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavigationComponent from './components/Nav';
+import CarouselComponent from './components/Carousel'; 
 import './App.css';
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <NavigationComponent/>
+        <CarouselComponent />
       </div>
     )
   }
