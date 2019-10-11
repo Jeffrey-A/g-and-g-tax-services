8e2b776ca6884dcd47bbfbb5ab21ba04e6b43b9e
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-10-10 22:03:33 -0400

    refactor Services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-function Services(props){
+class Services extends React.Component {
+  render() {
     return (
-        <div className="container">
-            <h3 className="section-heading">Services</h3>
-            <div className="services-container">
-                <div className="service text-center">Placeholder</div>
-                <div className="service text-center">Placeholder</div>
-                <div className="service text-center">Placeholder</div>
-            </div>
+      <div className="container">
+        <h3 className="section-heading">Services</h3>
+        <div className="services-container">
+          <div className="service text-center">Placeholder</div>
+          <div className="service text-center">Placeholder</div>
+          <div className="service text-center">Placeholder</div>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
 export default Services;
