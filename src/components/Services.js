35b85bd3f5a6f01420f050dfbe55956342a7efe6
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-10-12 23:24:55 -0400

    Fix services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,42 +4,88 @@ class Services extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isExpanded: false,
-      services: ["services 1", "services 2", "services 3"]
+      isExpanded1: false,
+      isExpanded2: false,
+      isExpanded3: false,
+      services1: ["services 1", "services 2", "services 3"],
+      services2: ["services 4", "services 5", "services 6"],
+      services3: ["services 7", "services 8", "services 9"]
     };
   }
 
-  componentDidMount() {
-    const services = ["services 1", "services 2", "services 3"]
-    const servs = [];
-    services.forEach((s, i) => servs.push(<li key={i}>{s}</li>));
-    this.setState({ services: servs });
-  }
+  createServices = services => {
+    const container = [];
+    services.forEach((serv, index) => {
+      container.push(<li key={index}>{serv}</li>);
+    });
+    return container;
+  };
 
-  handleClick = () => {
+  handleClick1 = () => {
     this.setState(preState => {
-      return { isExpanded: !preState.isExpanded };
+      return { isExpanded1: !preState.isExpanded1 };
+    });
+  };
+
+  handleClick2 = () => {
+    this.setState(preState => {
+      return { isExpanded2: !preState.isExpanded2 };
+    });
+  };
+
+  handleClick3 = () => {
+    this.setState(preState => {
+      return { isExpanded3: !preState.isExpanded3 };
     });
   };
 
   render() {
-    const { isExpanded, services } = this.state;
+    const {
+      isExpanded1,
+      isExpanded2,
+      isExpanded3,
+      services1,
+      services2,
+      services3
+    } = this.state;
     return (
       <div className="container">
         <h3 className="section-heading">Services</h3>
         <div className="services-container">
           <div
             className="service text-center"
-            onClick={() => this.handleClick()}
+            onClick={() => this.handleClick1()}
           >
             Placeholder
           </div>
-          {isExpanded ? services : null}
-          <div className="service text-center" onClick={this.handleClick}>
+          <div className="services-container">
+            <ul className="services-ul">
+              {isExpanded1 ? this.createServices(services1) : null}
+            </ul>
+          </div>
+
+          <div
+            className="service text-center"
+            onClick={() => this.handleClick2()}
+          >
             Placeholder
           </div>
-          <div className="service text-center" onClick={this.handleClick}>
+          <div className="services-container">
+            <ul className="services-ul">
+              {isExpanded2 ? this.createServices(services2) : null}
+            </ul>
+          </div>
+
+          <div
+            className="service text-center"
+            onClick={() => this.handleClick3()}
+          >
             Placeholder
+          </div>
+          <div className="services-container">
+            <ul className="services-ul">
+              {isExpanded3 ? this.createServices(services3) : null}
+            </ul>
           </div>
         </div>
       </div>
