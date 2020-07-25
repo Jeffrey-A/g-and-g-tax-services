dc998ce1b168dd80f8697789d178d3bb164e14c8
jalmanzar <jalmanzar@rubensteintech.com>
2020-07-25 16:42:29 -0400

    feat: use services icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,12 @@
 import React from "react";
+// Services image
+import IndividualAndBusinessTaxes from "../assets/taxes-individual-and-business.png";
+import itinRequest from "../assets/itin-request.png";
+import installmentPlan from "../assets/installement-plan.png";
+import businessLicense from "../assets/business-licenses.png";
+import bookkeeping from "../assets/bookkeeping.png";
+import taxId from "../assets/tax-id.png";
+import payrollAndSales from "../assets/payroll-and-sales-taxes.png";
 
 class Services extends React.Component {
   constructor(props) {
@@ -6,7 +14,7 @@ class Services extends React.Component {
     this.state = {};
   }
 
-  createServices = services => {
+  createServices = (services) => {
     const container = [];
     services.forEach((serv, index) => {
       container.push(<li key={index}>{serv}</li>);
@@ -19,86 +27,67 @@ class Services extends React.Component {
       <div id="services" className="services-section container-section">
         <div className="container">
           <h3 className="section-heading text-center">Our Services</h3>
+          <i className="fa fa-check-circle"></i>
           <div className="services-container">
-            <div className="service-card shadow  text-center">
-              <div className="service  service-category">
-                <p className="service-category-title">Placeholder</p>
+            <div className="service-card text-center">
+              <div className="service-card-img-container">
+                <img src={IndividualAndBusinessTaxes} alt="" />
               </div>
-
-              <ul className="services-ul">
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
-              </ul>
+              <p className="service-name">
+                income tax individual &#38; business
+              </p>
             </div>
 
-            <div className="service-card shadow text-center">
-              <div className="service service-category">
-                <p className="service-category-title">Placeholder</p>
+            <div className="service-card text-center">
+              <div className="service-card-img-container">
+                <img src={itinRequest} alt="" />
               </div>
-
-              <ul className="services-ul">
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
-              </ul>
+              <p className="service-name">itin request</p>
             </div>
 
-            <div className="service-card shadow text-center">
-              <div className="service service-category">
-                <p className="service-category-title">Placeholder</p>
+            <div className="service-card text-center">
+              <div className="service-card-img-container">
+                <img src={installmentPlan} alt="" />
               </div>
-
-              <ul className="services-ul">
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
-              </ul>
+              <p className="service-name">installment plans</p>
             </div>
 
-            <div className="service-card shadow text-center">
-              <div className="service service-category">
-                <p className="service-category-title">Placeholder</p>
+            <div className="service-card text-center">
+              <div className="service-card-img-container">
+                <img src={businessLicense} alt="" />
               </div>
-
-              <ul className="services-ul">
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
-              </ul>
+              <p className="service-name">business licenses</p>
             </div>
 
-            <div className="service-card shadow text-center">
-              <div className="service service-category">
-                <p className="service-category-title">Placeholder</p>
+            <div className="service-card text-center">
+              <div className="service-card-img-container">
+                <img src={bookkeeping} alt="" />
               </div>
-
-              <ul className="services-ul">
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
-              </ul>
+              <p className="service-name">bookkeeping</p>
             </div>
 
-            <div className="service-card shadow text-center">
-              <div className="service service-category">
-                <p className="service-category-title">Placeholder</p>
+            <div className="service-card text-center">
+              <div className="service-card-img-container">
+                <img src={taxId} alt="" />
               </div>
+              <p className="service-name">tax ID</p>
+            </div>
 
-              <ul className="services-ul">
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
-              </ul>
+            <div className="service-card text-center">
+              <div className="service-card-img-container">
+                <img src={payrollAndSales} alt="" />
+              </div>
+              <p className="service-name">payroll &#38; sales taxes</p>
             </div>
           </div>
 
           <div className="mk-appointment-container">
-            
-              <a className="mk-appointment" href="mailto:ggtaxservicesinc@gmail.com">
-                Make an Appointment
-              </a>
-           
+            <a
+              className="mk-appointment"
+              href="mailto:ggtaxservicesinc@gmail.com"
+            >
+              Make an Appointment
+            </a>
           </div>
         </div>
       </div>
